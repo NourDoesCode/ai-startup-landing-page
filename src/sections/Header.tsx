2d6ec5c440a8032a7806f141ae4b98619b2965c0
Nour <nourpow@gmail.com>
2024-08-09 17:41:44 +0100

    update header and logo section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,9 +5,11 @@ import Button from "@/components/Button";
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10 ">
+    <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur md:hidden"></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-4  rounded-xl  backdrop-blur ">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-4  rounded-xl  md:backdrop-blur relative">
+          <div className="absolute inset-0 backdrop-blur -z-10"></div>
           {/* Logo */}
           <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
             <LogoIcon className="h-8 w-8" />
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -7,15 +7,15 @@ import echoLogo from "@/assets/logo-echo.png";
 
 export const LogoTicker = () => {
   return (
-    <section className="py-20">
+    <section className="py-20 md:py-24">
       <div className="container">
-        <div className="flex items-center ">
-          <div className="flex-1 ">
+        <div className="flex items-center gap-5 ">
+          <div className="flex-1 md:flex-none ">
             <h2>Trusted by top innovative teams</h2>
           </div>
 
           {/* Companies and their logos */}
-          <div className=" flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <div className=" flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)] ">
             <div className="flex flex-none gap-14 ">
               {[
                 acmeLogo,
