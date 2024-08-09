ccf4ae06dc8c2a61bfa61d33801e9e464f2dc1b3
Nour <nourpow@gmail.com>
2024-08-09 16:28:18 +0100

    trusted by section mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
 import Head from "next/head";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Header />
       <Hero />
+      <LogoTicker />
     </>
   );
 }
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,42 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import apexLogo from "@/assets/logo-apex.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import echoLogo from "@/assets/logo-echo.png";
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+  return (
+    <section className="py-20">
+      <div className="container">
+        <div className="flex items-center ">
+          <div className="flex-1 ">
+            <h2>Trusted by top innovative teams</h2>
+          </div>
+
+          {/* Companies and their logos */}
+          <div className=" flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <div className="flex flex-none gap-14 ">
+              {[
+                acmeLogo,
+                apexLogo,
+                celestialLogo,
+                quantumLogo,
+                pulseLogo,
+                echoLogo,
+              ].map((logo) => (
+                <img
+                  src={logo.src}
+                  key={logo.src}
+                  className="h-[24px] w-auto"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
+
+//border-4 border-green-600
